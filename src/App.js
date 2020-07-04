c308eb16c4e3ddcd5593811c084c7bb0c43d1d27
mohammed sayeed <click2saeed@gmail.com>
2020-07-04 23:46:33 +0400

    added change and submit functionality

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ import TodoList from './components/TodoList'
 
 class App extends Component {
   state = {
-    items: [
-      {id:1, title: 'wake up'},
-      { id: 2, title: 'make breakfast' }
-    ],
+    items: [],
     id: uuid(),
     item: '',
     editItem: false
   };
 
   handleChange = (e) => {
-    console.log('handle change');
+    this.setState({
+      item: e.target.value
+    });
   }
 
-  handleSubmit = (e) => {
-    console.log('handle submit');
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    }
+    const updatedItems = [...this.state.items, newItem]
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuid(),
+      editItem: false
+    }, () => console.log(this.state))
   }
 
   clearList = () => {
@@ -37,7 +47,6 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state)
     return(
       <div className="container">
         <div className="row">
